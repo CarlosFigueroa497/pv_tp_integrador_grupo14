921b89b310e168c925c299edd14989c2685d6bb8
juanicolas619 <juanicolas619@gmail.com>
2025-06-17 00:28:17 -0300

    Agregue Header y Footer incorporando estilos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer
+  className="text-white text-center py-3 mt-auto"
+  style={{ backgroundColor: '#222', borderTop: '1px solid #555' }}
+>
+  <small><b>Integrantes: Figueroa Carlos, Briones Juan, Tapia Moraña Hernan | Trabajo Integrador © 2025 | UNJU</b></small>
+</footer>
+  );
+}
+
+export default Footer;
