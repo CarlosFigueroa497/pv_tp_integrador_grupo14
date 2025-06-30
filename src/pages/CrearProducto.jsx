7fa7bb5c02a0468511cfe3e653db736910ba9d77
juanicolas619 <juanicolas619@gmail.com>
2025-06-30 17:25:59 -0300

    Arreglo de boton de favoritos. Se creo un icono reemplazando al boton. Se añade mensajes de notificacion al crear o editar productos. El formulario de alta/edicion de producto ya no ocupa toda la pantalla

--- src/pages/CrearProducto.jsx
+++ src/pages/CrearProducto.jsx
@@ -1,17 +1,28 @@
 import ProductForm from '../components/ProductForm';
 import { useProductos } from '../context/ProductosContext'; 
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 
 function CrearProducto() {
   const { agregarProducto } = useProductos(); 
   const navigate = useNavigate();
 
-  const handleCrear = (nuevoProducto) => {
-    agregarProducto(nuevoProducto); // agrega a la lista global
-    navigate('/');
+    const handleCrear = (nuevoProducto) => {
+    agregarProducto(nuevoProducto);
+    toast.success('Producto creado con éxito!');  // <- notificación aquí
+    setTimeout(() => {
+  navigate('/');
+}, 1000);
   };
 
-  return <ProductForm onSubmit={handleCrear} />;
+  return (
+    <div className="container my-5 d-flex justify-content-center">
+      <div className="w-100" style={{ maxWidth: '600px' }}>
+        <ProductForm onSubmit={handleCrear} />
+      </div>
+    </div>
+  );
 }
 
 export default CrearProducto;
